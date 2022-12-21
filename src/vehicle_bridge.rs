5fc0e76f9c092462906652ecdbb169ce85d5bc59
ChenYing Kuo <evshary@gmail.com>
2022-12-21 16:48:12 +0800

    Add the first version to control the vehicle.

--- src/vehicle_bridge.rs
+++ src/vehicle_bridge.rs
@@ -2,18 +2,21 @@ use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
 use zenoh::prelude::sync::*;
+use zenoh::subscriber::Subscriber;
+use zenoh::buffers::reader::HasReader;
 use cdr::{CdrLe, Infinite};
 use carla::client::{Vehicle, ActorBase};
 use carla::rpc::VehicleWheelLocation;
 use crate::autoware_type;
 
-pub struct VehicleBridge {
+pub struct VehicleBridge<'a> {
     _vehicle_name: String,
     _actor: Vehicle,
+    _subscriber_control_cmd: Subscriber<'a, ()>
 }
 
-impl VehicleBridge {
-    pub fn new(z_session: Arc<Session>, name: String, actor: Vehicle) -> VehicleBridge {
+impl<'a> VehicleBridge<'a> {
+    pub fn new(z_session: Arc<Session>, name: String, actor: Vehicle) -> VehicleBridge<'a> {
         let publisher_velocity = z_session
             // TODO: Check whether Zenoh can receive the message
             .declare_publisher(name.clone()+"/rt/vehicle/status/velocity_status")
@@ -37,13 +40,49 @@ impl VehicleBridge {
             };
             let encoded = cdr::serialize::<_, _, CdrLe>(&velocity_msg, Infinite).unwrap();
             publisher_velocity.put(encoded).res().unwrap();
+            println!("{}", velocity_msg.longitudinal_velocity);
             // TODO: Check the published rate
             thread::sleep(Duration::from_millis(1000));
             //thread::sleep(Duration::from_millis(33)); // 30Hz
         });
+        let mut vehicle_actor = actor.clone();
+        let subscriber_control_cmd = z_session
+            .declare_subscriber(name.clone()+"/rt/external/selected/control_cmd")
+            .callback_mut(move |sample| {
+                match cdr::deserialize_from::<_, autoware_type::AckermannControlCommand, _>(sample.payload.reader(), cdr::size::Infinite) {
+                    Ok(cmd) => {
+                        let mut control = vehicle_actor.control();
+                        // TODO: how to match acceleration to throttle and brake
+                        control.throttle = if cmd.longitudinal.acceleration > 0.0 { cmd.longitudinal.acceleration / 2.0 } else { 0.0 };
+                        control.brake = if cmd.longitudinal.acceleration < 0.0 { -cmd.longitudinal.acceleration / 2.0 } else { 0.0 };
+                        // TODO: 0.3925 means 22.5 deg, but we should get the maximum steering degree first
+                        control.steer = -cmd.lateral.steering_tire_angle / 0.3925; // need to reverse the direction
+                        vehicle_actor.apply_control(&control);
+                        println!("throttle: {}, break: {}\r", control.throttle, control.brake);
+                    },
+                    Err(_) => {},
+                }
+            })
+            .res()
+            .unwrap();
+        let _subscriber_gate_mode = z_session
+            .declare_subscriber(name.clone()+"/rt/control/gate_mode_cmd")
+            .callback_mut(move |_| {
+
+            })
+            .res()
+            .unwrap();
+        let _subscriber_gear_cmd = z_session
+            .declare_subscriber(name.clone()+"/rt/external/selected/gear_cmd")
+            .callback_mut(move |_| {
+
+            })
+            .res()
+            .unwrap();
         VehicleBridge { 
             _vehicle_name: name,
             _actor: actor,
+            _subscriber_control_cmd: subscriber_control_cmd,
         }
     }
 }
