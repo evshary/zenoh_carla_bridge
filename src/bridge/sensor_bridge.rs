a78eca22e9518448e160cf1e6f58bb8d59fc573f
ChenYing Kuo <evshary@gmail.com>
2023-01-10 17:31:07 +0800

    Able to get which vehicle sensors belong.

--- src/bridge/sensor_bridge.rs
+++ src/bridge/sensor_bridge.rs
@@ -1,21 +1,53 @@
 use super::actor_bridge::ActorBridge;
 use crate::error::Result;
-use carla::{client::Sensor, prelude::*};
+use carla::{carla_sys::carla::sensor::data::LidarMeasurement, client::Sensor, prelude::*};
 use log::info;
 use r2r::builtin_interfaces::msg::Time;
 use zenoh::prelude::sync::*;
 
+pub enum SensorType {
+    CameraRgb,
+    LidarRayCast,
+    LidarRayCastSemantic,
+    Imu,
+    Collision,
+    NotSupport,
+}
+
 pub struct SensorBridge {
-    _sensor_type: String,
+    vehicle_name: String,
+    sensor_type: SensorType,
     _actor: Sensor,
 }
 
 impl SensorBridge {
     pub fn new(_z_session: &Session, actor: Sensor) -> Result<SensorBridge> {
-        let sensor_type = actor.type_id();
-        info!("Detect sensors {sensor_type}");
+        let vehicle_name = actor
+            .parent()
+            .unwrap()
+            .attributes()
+            .iter()
+            .find(|attr| attr.id() == "role_name")
+            .unwrap()
+            .value_string();
+        let sensor_type_id = actor.type_id();
+        info!("Detect sensors {sensor_type_id} from {vehicle_name}");
+        let sensor_type = match sensor_type_id.as_str() {
+            "sensor.camera.rgb" => {
+                actor.listen(|data| {
+                    //lidar_callback(data.try_into().unwrap());
+                });
+                SensorType::CameraRgb
+            }
+            "sensor.lidar.ray_cast" => SensorType::LidarRayCast,
+            "sensor.lidar.ray_cast_semantic" => SensorType::LidarRayCastSemantic,
+            "sensor.other.imu" => SensorType::Imu,
+            "sensor.other.collision" => SensorType::Collision,
+            _ => SensorType::NotSupport,
+        };
         Ok(SensorBridge {
-            _sensor_type: sensor_type,
+            vehicle_name,
+            sensor_type,
             _actor: actor,
         })
     }
@@ -26,3 +58,5 @@ impl ActorBridge for SensorBridge {
         Ok(())
     }
 }
+
+fn lidar_callback(measure: LidarMeasurement) {}
