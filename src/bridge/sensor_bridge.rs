8dac3c799c67989e6cba8312e9e1973c9c7b2049
ChenYing Kuo <evshary@gmail.com>
2023-01-12 11:46:01 +0800

    Port Jerry's lidar callback code.

--- src/bridge/sensor_bridge.rs
+++ src/bridge/sensor_bridge.rs
@@ -1,8 +1,20 @@
 use super::actor_bridge::ActorBridge;
 use crate::error::Result;
-use carla::{client::Sensor, prelude::*, sensor::data::LidarMeasurement};
+use crate::types::PointFieldType;
+use crate::utils;
+use carla::{
+    client::Sensor,
+    geom::Location,
+    prelude::*,
+    sensor::data::{LidarDetection, LidarMeasurement},
+};
+use cdr::{CdrLe, Infinite};
 use log::info;
-use r2r::builtin_interfaces::msg::Time;
+use r2r::{
+    builtin_interfaces::msg::Time,
+    sensor_msgs::msg::{PointCloud2, PointField},
+    std_msgs::msg::Header,
+};
 use std::sync::Arc;
 use zenoh::{prelude::sync::*, publication::Publisher};
 
@@ -33,19 +45,19 @@ impl SensorBridge {
             .value_string();
         let sensor_type_id = actor.type_id();
         info!("Detect sensors {sensor_type_id} from {vehicle_name}");
+
         let sensor_type = match sensor_type_id.as_str() {
             "sensor.camera.rgb" => SensorType::CameraRgb,
             "sensor.lidar.ray_cast" => {
-                let z_publisher = z_session
+                let pcd_publisher = z_session
                     .declare_publisher(format!(
                         "{vehicle_name}/rt/sensing/lidar/top/pointcloud_raw"
                     ))
                     .res()
                     .unwrap();
                 actor.listen(move |data| {
-                    lidar_callback(data.try_into().unwrap(), &z_publisher);
-                    // This line fails
-                    //z_publisher.put("Error").res().unwrap();
+                    let header = utils::create_ros_header().unwrap();
+                    lidar_callback(header, data.try_into().unwrap(), &pcd_publisher);
                 });
                 SensorType::LidarRayCast
             }
@@ -68,4 +80,60 @@ impl ActorBridge for SensorBridge {
     }
 }
 
-fn lidar_callback(_measure: LidarMeasurement, _publisher: &Publisher) {}
+fn lidar_callback(header: Header, measure: LidarMeasurement, pcd_publisher: &Publisher) {
+    let lidar_data = measure.as_slice();
+    if lidar_data.is_empty() {
+        return;
+    }
+    let point_step = std::mem::size_of_val(&lidar_data[0]) as u32;
+    let row_step = lidar_data.len() as u32;
+    let data: Vec<_> = lidar_data
+        .iter()
+        .flat_map(
+            |&LidarDetection {
+                 point: Location { x, y, z },
+                 intensity,
+             }| { [x, y, z, intensity] },
+        )
+        .flat_map(|elem| elem.to_ne_bytes())
+        .collect();
+    let fields = vec![
+        PointField {
+            name: "x".to_string(),
+            offset: 0,
+            datatype: PointFieldType::FLOAT32 as u8,
+            count: 1,
+        },
+        PointField {
+            name: "y".to_string(),
+            offset: 4,
+            datatype: PointFieldType::FLOAT32 as u8,
+            count: 1,
+        },
+        PointField {
+            name: "z".to_string(),
+            offset: 8,
+            datatype: PointFieldType::FLOAT32 as u8,
+            count: 1,
+        },
+        PointField {
+            name: "intensity".to_string(),
+            offset: 12,
+            datatype: PointFieldType::FLOAT32 as u8,
+            count: 1,
+        },
+    ];
+    let lidar_msg = PointCloud2 {
+        header,
+        height: 1,
+        width: lidar_data.len() as u32,
+        fields,
+        is_bigendian: utils::is_bigendian(),
+        point_step,
+        row_step,
+        data,
+        is_dense: true,
+    };
+    let encoded = cdr::serialize::<_, _, CdrLe>(&lidar_msg, Infinite).unwrap();
+    pcd_publisher.put(encoded).res().unwrap();
+}
